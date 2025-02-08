44d6098fc6c9b1e800933b882ee803c119345c29
Andrii Fedosiuk <afedosiuk@deloitte.com>
2025-02-08 15:07:32 +0200

    Add project files.

---
+++ Controllers/PlatformsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsServeice.Controllers;
+
+[ApiController, Route("api/commands/[controller]")]
+public class PlatformsController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult TestInboundConnection()
+    {
+        Console.WriteLine("--> Inbound Post # Command Servicу");
+
+        return Ok();
+    }
+}
